ff88cd638ed0d6718b320b56255a0b63a7290a5c
mitsuyar <mitsuyar@bxscience.edu>
2017-11-08 20:52:32 -0500

    Tut8 gives path error

--- batch.py
+++ batch.py
@@ -7,22 +7,9 @@ Contributors: salvadordura@gmail.com
 """
 from netpyne.batch import Batch
 import numpy as np
+from netpyne import specs
 
 
-def createBatch(params):
-    b = Batch(netParamsFile='M1_detailed.py')
-    for k,v in params.iteritems():
-        b.params.append({'label': k, 'values': v})
-    return b
-
-def runBatch(b, label):
-    b.batchLabel = label
-    b.saveFolder = 'data/'+b.batchLabel
-    b.method = 'grid'
-    b.runCfg = {'type': 'mpi',
-        'script': 'init.py',
-            'skip': True}
-    b.run()
 
 
 """
@@ -42,35 +29,29 @@ params[('NetStimEPT', 'loc')] = [0.3, 0.6, 0.9]
 
 #Exploring all segs
 
-params={}
 
-def weightNormE(pops=['PT_L5B'],
-                segs = None, allSegs = True, rule = 'PT_full', weights=list(np.arange(0.01, 0.2, 0.01)/100.0)):
-    # Add params
-    from cfg import cfg
-    from M1_detailed import netParams
-    excludeSegs = ['axon']
-    if not segs:
-        secs = []
-        locs = []
-        for secName,sec in netParams.cellParams[rule]['secs'].iteritems():
-            if secName not in excludeSegs:
-                if allSegs:
-                    nseg = sec['geom']['nseg']
-                    for iseg in range(nseg):
-                        secs.append(secName)
-                        locs.append((iseg+1)*(1.0/(nseg+1)))
-                else:
-                    secs.append(secName)
-                    locs.append(0.5)
+#Define weightNormE function
 
 
-    params[('NetStim1', 'pop')] = pops
-    params[('NetStim1', 'secList')] = secs
-    params[('NetStim1', 'loc')] = locs
-    params[('NetStim1', 'weight')] = weights
-    params[('NetStim1', 'numStims')] = 1
-    groupedParams = [('NetStim1', 'sec'), ('NetStim1', 'loc')]
+
+
+
+"""
+def createBatch(params):
+    b = Batch(netParamsFile='M1_detailed.py', groupedParams=groupedParams)
+    for k,v in params.iteritems():
+        b.params.append({'label': k, 'values': v})
+    return b
+"""
+#Define batchrun
+def runBatch(b, label):
+    b.batchLabel = label
+    b.saveFolder = 'data/'+b.batchLabel
+    b.method = 'grid'
+    b.runCfg = {'type': 'mpi',
+        'script': 'init.py',
+            'skip': True}
+    b.run()
 
 """
     initCfg = {}
@@ -88,13 +69,61 @@ def weightNormE(pops=['PT_L5B'],
     #initCfg[('GroupNetStimW1', 'pop')] = 'None'
     initCfg['addIClamp'] = 0
 """
-weightNormE(pops=['PT_L5B'],
-            segs = None, allSegs = True, rule = 'PT_full', weights=list(np.arange(0.01, 0.2, 0.01)/100.0))
 
-b = createBatch(params)
 
-runBatch(b,"Data4")
+#weights=list(np.arange(0.01, 0.2, 0.01)/100.0))
+#b = createBatch(params)
+#b = Batch(params=params, netParamsFile='netParams_cell.py', cfgFile='cfg_cell.py', initCfg=initCfg, groupedParams=groupedParams)
 
+
+
+secs = []
+locs = []
+pops=['PT_L5B']
+weights = [0.01]
+
+def weightNormE(pops=['PT_L5B'],
+                segs = None, allSegs = True, rule = 'PT_full', weights= [0.01]):
+    # Add params
+    from cfg import cfg
+    from M1_detailed import netParams
+    excludeSegs = ['axon']
+    if not segs:
+        for secName,sec in netParams.cellParams[rule]['secs'].iteritems():
+            if secName not in excludeSegs:
+                if allSegs:
+                    nseg = sec['geom']['nseg']
+                    for iseg in range(nseg):
+                        secs.append(secName)
+                        locs.append((iseg+1)*(1.0/(nseg+1)))
+                else:
+                    secs.append(secName)
+                    locs.append(0.5)
+
+
+
+weightNormE(pops= pops,
+                segs = None, allSegs = True, rule = 'PT_full', weights= [0.01])
+
+params = specs.ODict()
+params[('NetStim1', 'pop')] = pops
+params[('NetStim1', 'sec')] = secs
+params[('NetStim1', 'loc')] = locs
+params[('NetStim1', 'weight')] = weights
+groupedParams = [('NetStim1', 'sec'), ('NetStim1', 'loc')]
+
+b = Batch(params=params, netParamsFile='M1_detailed.py', cfgFile='cfg.py',groupedParams=groupedParams)
+
+
+
+
+
+
+    
+
+
+
+runBatch(b,"Data 4")
 
 
 
